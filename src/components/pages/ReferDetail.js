fac9b66396a045ed3aaedbeb139b0f3dba4cdbc6
ohserin <97070004+ohserin@users.noreply.github.com>
2022-04-21 14:14:54 +0900

    22.04.21

--- src/components/pages/ReferDetail.js
+++ src/components/pages/ReferDetail.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
 import { gsap } from "gsap";
 
-class ReferDetail extends React.Component {
-    componentDidMount(){
+// function ReferDetail(props) {
+//     console.log(props);
+//     return (
+//         <div>ReferDetail</div>
+//     )
+// }
 
-        document.querySelector("body").style.background = "#F0EEEB";
+class ReferDetail extends React.Component {
+    componentDidMount() {
+        const {location, history} = this.props;
+        if(location.state === undefined) {
+            history.push("/reference");
+        }
         this.mainAnimation();
     }
 
@@ -22,33 +31,67 @@ class ReferDetail extends React.Component {
                 bottom: 0,
                 delay: 0.2,
             });
-            gsap.to(".refer__inner", {
-                duration: 0.7,
-                y: 0,
-                opacity: 1,
-                delay: 1.5,
-                ease: "power4.out"
-            });
         }, 10)
     }
 
-    render (){
-        return (
-            <>
-            <Header color="light" />
-            <Contents>
-                <section className="refer__cont light">
-                    <div className="container">
-                        <div className="refer__inner">
-                            ddd
-                        </div>
-                    </div>
-                </section>
-            </Contents>
-            <Footer color="light" />
-        </>
-        )
+
+    render() {
+        const {location} = this.props;
+        console.log(location.state);
+
+        if(location.state === undefined){
+            return <div>잘못된 페이지 입니다.</div>
+        } else {
+            return (
+                <>
+                <Header color="light" />
+                <Contents>
+                    <section className="refer__cont">
+                        <div className="container">
+                            <div className="refer__inner">
+                                <div className="refer__table">
+                                 <h3>{location.state.title}</h3>
+                                 <p>{location.state.desc}</p>
+                                     <table className="table">
+                                         <thead>
+                                             <tbody>
+                                                 <tr>
+                                                     <th>특징</th>
+                                                     <th>설명</th>
+                                                 </tr>
+                                             </tbody>
+                                             <tr>
+                                                 <td>요소</td>
+                                                 <td>{location.state.element}</td>
+                                             </tr>
+                                             <tr>
+                                                 <td>닫는 태그</td>
+                                                 <td>{location.state.tag}</td>
+                                             </tr>
+                                             <tr>
+                                                 <td>버전</td>
+                                                 <td>{location.state.version}</td>
+                                             </tr>
+                                             <tr>
+                                                 <td>시각적표현</td>
+                                                 <td>{location.state.view}</td>
+                                             </tr>
+                                             <tr>
+                                                 <td>사용성</td>
+                                                 <td>{location.state.use}</td>
+                                             </tr>
+                                         </thead>
+                                     </table>
+                                 </div>
+                                </div>
+                            </div>
+                    </section>
+                </Contents>
+                <Footer color="light" />
+                </>
+            )
+        }
     }
 }
 
-export default ReferDetail
+export default ReferDetail;
