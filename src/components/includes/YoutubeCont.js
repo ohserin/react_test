808b39c17477e777951f9cab79a725fd4ce2f105
ohserin <97070004+ohserin@users.noreply.github.com>
2022-04-25 14:15:28 +0900

    22.04.25

--- src/components/includes/YoutubeCont.js
+++ src/components/includes/YoutubeCont.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
+import YoutubeList from "../includes/YoutubeList";
 
-function YoutubeCont() {
+function YoutubeCont(props) {
   return (
     <section className="youtube__cont">
       <div className="container">
         <div className="youtube__inner">
-          
+          <YoutubeList items={props.lists}  />
         </div>
       </div>
     </section>
   )
 }
-
-export default YoutubeCont
+export default YoutubeCont;
--- src/components/pages/Youtube.js
+++ src/components/pages/Youtube.js
@@ -5,72 +5,90 @@ import Footer from "../layout/Footer";
 import Title from "../layout/Title";
 import Contact from "../layout/Contact";
 import YoutubeCont from "../includes/YoutubeCont";
+// import YoutubeSearch from "../includes/YoutubeSearch";
+// import YoutubeList from "../includes/YoutubeList";
 import Loading from "../basics/Loading";
+
 import { gsap } from "gsap";
+import axios from "axios";
 
 class Youtube extends React.Component {
-state = {
-    isLoading: true,
-}
+    state = {
+        isLoading: true,
+        lists: [],
+        searchs: []
+    }
 
-mainAnimation = () => {
-    setTimeout(() => {
-        gsap.to("#header", {
-            duration: 0.8, 
-            top: 0,
-        });
-        gsap.to("#footer", {
-            duration: 0.8, 
-            bottom: 0,
-            delay: 0.2,
-        });
-        gsap.to(".youtube__inner", {
-            duration: 0.7,
-            y: 0,
-            opacity: 1,
-            delay: 1.0,
-            ease: "power4.out"
-        });
-    }, 10)
-}
+    mainAnimation = () => {
+        setTimeout(() => {
+            gsap.to("#header", {
+                duration: 0.8,
+                top: 0,
+            });
+            gsap.to("#footer", {
+                duration: 0.8,
+                bottom: 0,
+                delay: 0.2,
+            });
+            gsap.to(".cont__title strong", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.0,
+                ease: "power4.out"
+            });
+            gsap.to(".cont__title em", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.3,
+                ease: "power4.out"
+            });
 
-getSite = () => {
-    setTimeout(() => {
-        console.log("두번째 시작")
-        this.setState({isLoading: false});
+            // gsap.to(".refer__inner", {
+            //     duration: 0.7,
+            //     y: 0,
+            //     opacity: 1,
+            //     delay: 1.6,
+            //     ease: "power4.out"
+           // });
+        }, 10)
+    }
+
+    getYoutubes = async () => {
+        const lists = await axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=28&key=AIzaSyBUStznmnSJruhaD0yJAwElLrwYteCAWdA&type=video");
+        console.log(lists)
+        this.setState({lists, isLoading: false});
         this.mainAnimation();
-    }, 1600)
-}
+    }
 
-componentDidMount(){
-    setTimeout(() => {
-        document.getElementById("loading").classList.remove("loading__active");
-        this.getSite();
-    }, 2000);
-}
+    componentDidMount(){
+        setTimeout(() => {
+            document.getElementById("loading").classList.remove("loading__active");
+            this.getYoutubes();
+        }, 2000);
+    }
 
+    render(){
+        const { isLoading, lists } = this.state;
+        return (
+            <>            {isLoading ? (
+                <Loading color="black" />
 
-render(){
-    const {isLoading} = this.state;
-
-    return (
-        <>
-            {isLoading ? (
-                <Loading />
             ) : (
-            <>
+                <>
             <Header />
                 <Contents>
-                    <Title title={["Youtube","book"]} />
-                    <YoutubeCont />
+                        <Title title={["Youtube", "reference"]} />
+                        <YoutubeCont lists={lists}/>
                     <Contact />
                 </Contents>
             <Footer />
             </>
             )}
         </>
-    )
-}
+        )
+    }
 }
 
 
